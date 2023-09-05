62e518f0e39df2c976c99f47dbdba886db22eb98
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-09-05 12:43:57 -0700

    landing mock up

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+const socials = {
+    facebook: "https://www.facebook.com/TheYardWorkShow",
+    twitter: "https://twitter.com/TheYardWorkShow",
+    instagram: "https://www.instagram.com/theyardworkshow/",
+    discord: "https://discord.com/invite/6RtX93jM8p",
+    tiktok: "https://www.tiktok.com/@theyardworkshow",
+    youtube: "https://www.youtube.com/channel/UCd2r61aFQ4NwdOsuaPJ_oxA",
+    spotify: "https://open.spotify.com/user/31wwtemf7lh6erzcusvbi5svc2ka?si=5fdef842d78e488b",
+    twitch: "https://www.twitch.tv/theyardworkshow",
+    patreon: "https://www.patreon.com/TheYardWorkShow",
+    riki: "https://www.twitch.tv/theechocolateprince",
+    kyle: "https://www.twitch.tv/ckpuffin"
+}
+
+export default socials
