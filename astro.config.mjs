a4776dcfc79e37423dc42234e568dc79c771ec86
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-09-05 12:16:23 -0700

    adds deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), mdx()],
+  output: "server",
+  adapter: vercel({
+    /* 
+      [@astrojs/vercel/serverless] Vercel's hosting plans might have limits to the number of functions you can create.
+      You can set functionPerRoute: false to prevent surpassing the limit.
+    */
+    functionPerRoute: false
+  })
+});
