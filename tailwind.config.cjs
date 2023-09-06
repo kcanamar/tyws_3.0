967e61f2e67493e64b77a11517d08e954f15984c
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-09-05 18:33:19 -0700

    updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				black: {
+					950: "#272727ff"
+				},
+				brown: {
+					950: "#EFD09EFF",
+					1000: "#D4AA7DFF"
+				},
+				blue: {
+					950: "#A6E1FAFF",
+					1000: "#3C91E6FF"
+				}
+			},
+		},
 	},
 	plugins: [],
 }
